9828901ac3a77f7c8d9981eea50d53fb88636a7e
Kings Michael <kingsmichael.official@gmail.com>
2026-07-27 20:50:34 +0100

    fix

--- sylvaline/wsgi.py
+++ sylvaline/wsgi.py
@@ -1,4 +1,12 @@
 import os
+import sys
+from pathlib import Path
+
+ROOT_DIR = str(Path(__file__).resolve().parent.parent)
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sylvaline.settings')
 application = get_wsgi_application()
